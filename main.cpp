a55dc3a22f25d9189440e59778fcef0076c31964
JS9002 <54071261+JS9002@users.noreply.github.com>
2020-12-22 12:50:02 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include "pml_hash.h"
+
+int main() {
+	PMLHash hash("/home/zwz/test/newfile");
+	for (uint64_t i = 1; i <= 17; i++) {
+	    hash.insert(i, i); 
+	}
+	cout << "Insert(1 ~ 17) OK!" << endl;
+	hash.print();
+	
+	for (uint64_t i = 18; i <= 33; i++) {
+	    hash.insert(i, i); 
+	}
+	cout << "Insert(18 ~ 33) OK!" << endl;
+	hash.print();
+	
+	for (uint64_t i = 34; i <= 35; i++) {
+	    hash.insert(i, i); 
+	}
+	cout << "Insert(34 ~ 35) OK!" << endl;
+	hash.print();
+
+	for (uint64_t i = 15; i <= 20; i++) {
+		uint64_t val;
+		hash.search(i, val);
+		cout << "Search(key: " << i << ")--> (value: " << val << ")" << endl;
+	}
+	
+	for (uint64_t i = 15; i <= 20; i++) {
+	    hash.remove(i);
+	}
+	cout << "Remove(15 ~ 20) OK!" << endl;
+	hash.print();
+	
+	for (uint64_t i = 25; i <= 30; i++) {
+	    hash.update(i, i+1);
+	    cout << "Update(" << i << ", " << i <<") -->" 
+		<< "(" << i << ", " << i+1 << ") OK!" << endl; 
+	}
+	hash.print();
+	return 0;
+}
